1f1f77b10b0e88e00da2ea46f5be43dd6e774780
Soon Wee <38690268+soonwee123@users.noreply.github.com>
2020-01-03 23:40:57 +0800

    Delete StateComparator.java

--- Project/StateComparator.java
+++
@@ -1,9 +0,0 @@
-import java.util.Comparator;
-
-class StateComparator implements Comparator<State> {
-
-    @Override
-    public int compare(State s1, State s2) {
-        return s1.getState() - s2.getState();
-    }
-}
